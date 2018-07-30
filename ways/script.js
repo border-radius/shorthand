278491249c630c621ebc2d17b1b6b5b7a5cfff55
border-radius <pharshmaster@gmail.com>
2018-07-30 19:43:54 +0300

    better script

--- ways/script.js
+++ ways/script.js
@@ -3,18 +3,24 @@ function request (proxy, data) {
         const rand = Array.apply(null, {
             length: Math.random() * 8 + 4
         }).map(i => String.fromCharCode(Math.random()*25+97)).join('');
+        const payload = encodeURIComponent(btoa(JSON.stringify(data)));
         const timeout = setTimeout(reject, 10000, new Error('Timeout'));
-        window[rand] = function (input) {
+        const script = document.createElement('script');
+        window[rand] = (input) => {
             try {
                 clearTimeout(timeout);
+                document.body.removeChild(script);
                 resolve(JSON.parse(input));
             } catch (e) {
                 reject(e);
             }
         };
-        const script = document.createElement('script');
-        const payload = encodeURIComponent(btoa(JSON.stringify(data)));
         script.src = [proxy, payload].join(['?', '='].join(rand));
+        script.onerror = e => {
+            clearTimeout(timeout);
+            document.body.removeChild(script);
+            reject(e);
+        };
         document.body.appendChild(script);
     });
 }
